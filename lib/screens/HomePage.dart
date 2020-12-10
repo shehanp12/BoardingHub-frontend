a68cfc21285e5292ba86802deaadce89b390582b
Anjana lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2020-12-10 18:21:11 +0530

    home-add

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/ads_list_data.dart';
+import 'package:flutter_app/screens/Ads_list_view.dart';
 import 'package:flutter_app/screens/bottom_navigation_view/bottom_bar_view.dart';
 import 'package:flutter_app/screens/bottom_navigation_view/app_theme.dart';
 import 'package:flutter_app/screens/bottom_navigation_view/tabIcon_data.dart';
+import 'package:flutter_app/screens/filter/filters_screen.dart';
 import 'package:flutter_app/screens/my_diary_screen.dart';
 import 'package:flutter_app/screens/training_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   final String pageTitle;
@@ -19,7 +23,7 @@ class _HomePageState extends State<HomePage>
   AnimationController animationController;
 
   List<TabIconData> tabIconsList = TabIconData.tabIconsList;
-
+  List<AdsListData> adList = AdsListData.adList;
   Widget tabBody = Container(
     color: BoardingAppTheme.background,
   );
@@ -57,7 +61,9 @@ class _HomePageState extends State<HomePage>
             } else {
               return Stack(
                 children: <Widget>[
-                  tabBody,
+                  getSearchBarUI(),
+                  SizedBox(height: 50,),
+                 
                   bottomBar(),
                 ],
               );
@@ -73,6 +79,34 @@ class _HomePageState extends State<HomePage>
     return true;
   }
 
+
+  Widget getHotelViewList() {
+    final List<Widget> hotelListViews = <Widget>[];
+    for (int i = 0; i < adList.length; i++) {
+      final int count = adList.length;
+      final Animation<double> animation =
+          Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: Interval((1 / count) * i, 1.0, curve: Curves.fastOutSlowIn),
+        ),
+      );
+      hotelListViews.add(
+        AdsListView(
+          callback: () {},
+          hotelData: adList[i],
+          animation: animation,
+          animationController: animationController,
+        ),
+      );
+    }
+    animationController.forward();
+    return Column(
+      children: hotelListViews,
+    );
+  }
+
+  
   Widget bottomBar() {
     return Column(
       children: <Widget>[
@@ -203,6 +237,81 @@ class _HomePageState extends State<HomePage>
           ),
         )
       ],
+    );
+  }
+
+  Widget getSearchBarUI() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16, top: 8, bottom: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: CardAppTheme.buildLightTheme().backgroundColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(38.0),
+                  ),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        offset: const Offset(0, 2),
+                        blurRadius: 8.0),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 16, right: 16, top: 4, bottom: 4),
+                  child: TextField(
+                    onChanged: (String txt) {},
+                    style: const TextStyle(
+                      fontSize: 18,
+                    ),
+                    cursorColor: CardAppTheme.buildLightTheme().primaryColor,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'London...',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: CardAppTheme.buildLightTheme().primaryColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(38.0),
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    offset: const Offset(0, 2),
+                    blurRadius: 8.0),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(32.0),
+                ),
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Icon(FontAwesomeIcons.search,
+                      size: 20,
+                      color: CardAppTheme.buildLightTheme().backgroundColor),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
