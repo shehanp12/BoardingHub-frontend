2bc13539a5003d94a626ed810417e5ea7dfbe66d
shehanp12 <shehanp068@gmail.com>
2020-12-12 04:52:51 +0530

    SignInPage.dart

--- lib/utils/rest_service.dart
+++ lib/utils/rest_service.dart
@@ -1,31 +1,20 @@
 import 'package:dio/dio.dart';
 
 class RestService {
-
-  Dio _dio  = new Dio();
+  Dio _dio = new Dio();
 
   final String host = 'http://192.168.8.172:5000/api/';
 
-   registerUser(boardingProvider) async{
-        try{
-          return await _dio.post(host+'user/register',data: {
-            "username":boardingProvider.username,
-            "fullName":boardingProvider.fullName,
-            "email":boardingProvider.email,
-            "password":boardingProvider.password
-
-          }
-
-
-          );
-
-
-        } on DioError catch(e) {
-          e.response.data['msg'];
-
-        }
-
-   }
-
+  registerUser(boardingProvider) async {
+    try {
+      return await _dio.post(host + 'user/register', data: {
+        "username": boardingProvider.username,
+        "fullName": boardingProvider.fullName,
+        "email": boardingProvider.email,
+        "password": boardingProvider.password
+      });
+    } on DioError catch (e) {
+      e.response.data['msg'];
+    }
+  }
 }
-
