d2478859f5cadcf7e8ae4011e828627b467c1bdb
shehanp12 <shehanp068@gmail.com>
2021-01-07 14:52:49 +0530

    BoardingProvider MODEL CHANGING

--- lib/models/BoardingProvider.dart
+++ lib/models/BoardingProvider.dart
@@ -4,19 +4,28 @@
 
 import 'dart:convert';
 
-BoardingProvider boardingProviderFromJson(String str) => BoardingProvider.fromJson(json.decode(str));
+BoardingProvider boardingProviderFromJson(String str) =>
+    BoardingProvider.fromJson(json.decode(str));
 
-String boardingProviderToJson(BoardingProvider data) => json.encode(data.toJson());
+String boardingProviderToJson(BoardingProvider data) =>
+    json.encode(data.toJson());
 
 class BoardingProvider {
-  BoardingProvider({
-    this.fullName,
-    this.email,
-    this.contactNumber,
-    this.userName,
-    this.address,
-    this.password,
-  });
+  BoardingProvider(
+    String fullName,
+    String email,
+    String contactNumber,
+    String userName,
+    String address,
+    String password,
+  ) {
+    this.fullName = fullName;
+    this.email = email;
+    this.contactNumber = contactNumber;
+    this.userName = userName;
+    this.address = address;
+    this.password = password;
+  }
 
   String fullName;
   String email;
@@ -25,21 +34,22 @@ class BoardingProvider {
   String address;
   String password;
 
-  factory BoardingProvider.fromJson(Map<String, dynamic> json) => BoardingProvider(
-    fullName: json["fullName"],
-    email: json["email"],
-    contactNumber: json["contactNumber"],
-    userName: json["userName"],
-    address: json["address"],
-    password: json["password"],
-  );
+  factory BoardingProvider.fromJson(Map<String, dynamic> json) =>
+      BoardingProvider(
+        json["fullName"],
+        json["email"],
+        json["contactNumber"],
+        json["userName"],
+        json["address"],
+        json["password"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "fullName": fullName,
-    "email": email,
-    "contactNumber": contactNumber,
-    "userName": userName,
-    "address": address,
-    "password": password,
-  };
+        "fullName": fullName,
+        "email": email,
+        "contactNumber": contactNumber,
+        "userName": userName,
+        "address": address,
+        "password": password,
+      };
 }
