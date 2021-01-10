11ee5dcfbed4425a1447095f3c076497668f073a
Anjana lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2021-01-10 23:11:38 +0530

    Update ProfilePage.dart

--- lib/screens/ProfilePage.dart
+++ lib/screens/ProfilePage.dart
@@ -127,10 +127,6 @@ class ProfileListItems extends StatelessWidget {
             text:getTranslated(context, 'My_Ads'),
           ),
           ProfileListItem(
-            icon: LineAwesomeIcons.history,
-            text:getTranslated(context,'Purchase_History'),
-          ),
-          ProfileListItem(
             icon: LineAwesomeIcons.question_circle,
             text:getTranslated(context,'Help_&_Support'),
           ),
