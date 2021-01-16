11802bb42f30682e0526128ff07be68fc10bc99c
Anjana lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2021-01-16 19:42:56 +0530

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class _MyAppState extends State<MyApp> {
               //home: AdsScreen(),
 
 
-              home: HomePage(),
+              home: SplashScreen(),
 
               routes: <String, WidgetBuilder>{
                 '/signup': (BuildContext context) => SignUpPage(),
